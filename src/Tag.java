35a639b48fa08dd02d798e3bb97aa5a00ae039ea
hongyang <hong.yang.code@gmail.com>
2021-08-10 14:21:02 +0800

    Added range object to tag

--- src/Tag.java
+++ src/Tag.java
@@ -3,6 +3,7 @@ import java.util.Objects;
 public class Tag {
     private String prefix;
     private String value;
+    private Range range;
 
     public String getPrefix() {
         return this.prefix;
@@ -12,17 +13,26 @@ public class Tag {
         return this.value;
     }
 
+    public Range getRange() {
+        return this.range;
+    }
+
+    public void setRange(Range range) {
+        this.range = range;
+    }
+
     @Override
     public boolean equals(Object tag) {
         if (tag == this) return true;
         if (!(tag instanceof Tag)) return false;
         Tag that = (Tag) tag;
-        return this.prefix.equals(that.prefix) && this.value.equals(that.value);
+        boolean isEquals = this.prefix.equals(that.prefix) && this.value.equals(that.value);
+        return  range == null ? isEquals : isEquals && this.range.equals(that.range);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(prefix, value);
+        return Objects.hash(this.prefix, this.value, this.range);
     }
 
     @Override
